05371a370749b191b90174370128d918ba381a24
IvanGriGri <igigcc@mail.ru>
2025-10-21 18:12:17 +0300

    Блок_03
    Переименованы файлы для удобства ориентирования

---
+++ Блок_03/4withRotateBF.py
@@ -0,0 +1,30 @@
+from pioneer_sdk import *
+import time
+
+mk=Pioneer()
+mk.arm()
+time.sleep(1)
+mk.takeoff()
+try:
+    mk.go_to_local_point_body_fixed(0, 0, 1.5, 0)
+    time.sleep(5)
+    mk.go_to_local_point_body_fixed(0, 1, 0, 0)
+    time.sleep(5)
+    mk.go_to_local_point_body_fixed(0, 0, 0, 3.14/2)
+    time.sleep(5)
+    mk.go_to_local_point_body_fixed(0, 1, 0, 0)
+    time.sleep(5)
+    mk.go_to_local_point_body_fixed(0, 0, 0, 3.14/2)
+    time.sleep(5)
+    mk.go_to_local_point_body_fixed(0, 1, 0, 0)
+    time.sleep(5)
+    mk.go_to_local_point_body_fixed(0, 0, 0, 3.14/2)
+    time.sleep(5)
+    mk.go_to_local_point_body_fixed(0, 1, 0, 0)
+    time.sleep(5)
+    mk.go_to_local_point_body_fixed(0, 0, 0, 3.14/2)
+    time.sleep(5)
+finally:
+    mk.land()
+    time.sleep(5)
+    mk.disarm()
